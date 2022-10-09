8413ffcee62bb9299ab493b2bd7231f5355062f4
KhuongNguyen <ndkhuong89@gmail.com>
2022-10-09 20:56:23 +0700

    List

---
+++ trenlop/09102022_list_tuple.py
@@ -0,0 +1,24 @@
+l = []
+for i in range(3):
+    l.append(int(input("Nhập phần tử thứ {} của list: ".format(i + 1))))
+print("Bạn đã nhập list: ")
+print(l)
+print("Độ dài list: ", len(l))
+
+max = min = l[0]
+for i in l:
+    if (i > max):
+        max = i
+    if (i < min):
+        min = i
+print("max = ", max)
+print("min = ", min)
+
+print("Sort l: ")
+l.sort()
+print(l)
+
+del l[2]
+print(l)
+
+del l
