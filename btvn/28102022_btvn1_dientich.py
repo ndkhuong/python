b189243a73d01dde0c6da96bb2ee710608e101a4
KhuongNguyen <ndkhuong89@gmail.com>
2022-10-30 00:04:07 +0700

    BTVN 29102022

---
+++ btvn/28102022_btvn1_dientich.py
@@ -0,0 +1,7 @@
+from mylib import dien_tich 
+
+dt = dien_tich()
+#dt.s_hbh()
+#dt.s_hinhthoi()
+#dt.s_hinhtru()
+dt.s_ngugiac()
--- trenlop/mylib.py
+++ trenlop/mylib.py
@@ -6,3 +6,17 @@ def nhan(a, b):
     return a*b
 def chia(a, b):
     return a/b
+
+class Person():
+    ten = ""
+    tuoi = 0
+    diachi = ""
+    def __init__(self, ten, tuoi, diachi):
+        self.ten = ten
+        self.tuoi = tuoi
+        self.diachi = diachi
+    def hienthi(self):
+        print("Ten: ", self.ten)
+        print("Tuoi: ", self.tuoi)
+        print("Dia chi: ", self.diachi)
+
