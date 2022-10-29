754246c6db38267dc5d0f6a5c910b67a29bf503c
KhuongNguyen <ndkhuong89@gmail.com>
2022-10-30 00:09:58 +0700

    BTVN 29102022

--- btvn/mylib.py
+++ btvn/mylib.py
@@ -1,3 +1,44 @@
+
+import math
+class dien_tich():
+    s = 0
+    def in_s(self):
+        print("Dien tich: {:.2f}".format(self.s))
+        
+    def s_hbh(self):
+        print("Tinh dien tich hinh binh hanh")
+        a = int(input("Nhap do dai canh day: "))
+        h = int(input("Nhap do dai chieu cao: "))
+        self.s = a * h
+        self.in_s()
+        return self.s
+        
+    def s_hinhtru(self):
+        print("Tinh dien tich hinh tru")
+        r = int(input("Nhap do dai ban kinh: "))
+        h = int(input("Nhap do dai chieu cao: "))
+        self.s = 2*3.14*r*r + 2*3.14*r*h
+        self.in_s()
+        return self.s
+        
+    def s_hinhthoi(self):
+        print("Tinh dien tich hinh thoi")
+        d1 = int(input("Nhap do dai duong cheo 1: "))
+        d2 = int(input("Nhap do dai duong cheo 2: "))
+        self.s = 0.5 * d1 * d2
+        self.in_s()
+        return self.s
+        
+    def s_ngugiac(self):
+        print("Tinh dien tich hinh ngu giac deu")
+        c = int(input("Nhap do dai canh: "))
+        self.s = 5*c*c/(4*math.sqrt(5 - 2*math.sqrt(5)))
+        self.in_s()
+        return self.s
+
+
+
+
 def ve_ban_co(xTot, yTot, xVua, yVua):
     m = 6
     n = 6
@@ -62,40 +103,4 @@ def s_hinh_vuong(a):
     return a * a
 
 
-import math
-class dien_tich():
-    s = 0
-    def in_s(self):
-        print("Dien tich: {:.2f}".format(self.s))
-        
-    def s_hbh(self):
-        print("Tinh dien tich hinh binh hanh")
-        a = int(input("Nhap do dai canh day: "))
-        h = int(input("Nhap do dai chieu cao: "))
-        self.s = a * h
-        self.in_s()
-        return self.s
-        
-    def s_hinhtru(self):
-        print("Tinh dien tich hinh tru")
-        r = int(input("Nhap do dai ban kinh: "))
-        h = int(input("Nhap do dai chieu cao: "))
-        self.s = 2*3.14*r*r + 2*3.14*r*h
-        self.in_s()
-        return self.s
-        
-    def s_hinhthoi(self):
-        print("Tinh dien tich hinh thoi")
-        d1 = int(input("Nhap do dai duong cheo 1: "))
-        d2 = int(input("Nhap do dai duong cheo 2: "))
-        self.s = 0.5 * d1 * d2
-        self.in_s()
-        return self.s
-        
-    def s_ngugiac(self):
-        print("Tinh dien tich hinh ngu giac deu")
-        c = int(input("Nhap do dai canh: "))
-        self.s = 5*c*c/(4*math.sqrt(5 - 2*math.sqrt(5)))
-        self.in_s()
-        return self.s
     
