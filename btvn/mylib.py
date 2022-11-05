ab6b117ae7ed55536af79c0099a407722a560ede
KhuongNguyen <ndkhuong89@gmail.com>
2022-11-05 14:46:34 +0700

    BTVN 29102022

--- btvn/mylib.py
+++ btvn/mylib.py
@@ -1,4 +1,99 @@
 
+class TruongThuySon():
+    vukhi = ''
+    tuoi = 0
+    def __init__(self, vukhi, tuoi):
+        self.vukhi = vukhi
+        self.tuoi = tuoi
+    def info(self):
+        print('Vu khi: ', self.vukhi)
+        print('Tuoi: ', self.tuoi)
+
+class TruongVoKy(TruongThuySon):
+    lev = 0
+    def __init__(self, vukhi, tuoi, lev):
+        super().__init__(vukhi, tuoi)
+        self.lev = lev
+    def infoTruongVoKy(self):
+        self.info()
+        print("Level: ", self.lev)
+
+class NhanVat():
+    def __init__(self, ten, tuoi, monphai, lev):
+        self.ten = ten
+        self.tuoi = tuoi
+        self.monphai = monphai
+        self.lev = lev
+    def info(self):
+        print("Ten: {}, Tuoi: {}, Mon phai: {}, Level: {}".format(self.ten, self.tuoi, self.monphai, self.lev))
+
+def in_list_nv(list_nv):
+    for nv in list_nv:
+        nv.info()
+
+def tim_nv_max_lev(list_nv):
+    nv_max = list_nv[0]
+    for nv in list_nv:
+        if nv.lev > nv_max.lev:
+            nv_max = nv
+    nv_max.info()
+    return nv_max
+
+def tim_nv_min_lev(list_nv):
+    nv_min = list_nv[0]
+    for nv in list_nv:
+        if nv.lev < nv_min.lev:
+            nv_min = nv
+    nv_min.info()
+    return nv_min
+
+def sap_xep_lev_tang(list_nv):
+    for i in range(len(list_nv)):
+        for j in range(len(list_nv) - i - 1):
+            if (list_nv[j].lev > list_nv[j+1].lev):
+                temp = list_nv[j]
+                list_nv[j] = list_nv[j+1]
+                list_nv[j+1] = temp
+    in_list_nv(list_nv)
+    return list_nv
+
+def sap_xep_lev_giam(list_nv):
+    for i in range(len(list_nv)):
+        for j in range(len(list_nv) - i - 1):
+            if (list_nv[j].lev < list_nv[j+1].lev):
+                temp = list_nv[j]
+                list_nv[j] = list_nv[j+1]
+                list_nv[j+1] = temp
+    in_list_nv(list_nv)
+    return list_nv
+
+def tim_nv_lev_lon_hon_5(list_nv):
+    for nv in list_nv:
+        if (nv.lev > 5):
+            nv.info()
+
+def tim_nv_monphai_vovinam(list_nv):
+    for nv in list_nv:
+        if (nv.monphai == 'VoViNam'):
+            nv.info()
+
+def nhap_x():
+    retry = True
+    while (retry == True):
+        try:
+            x = int(input("Nhap so thu tu cua chuc nang: "))
+            if (x < 2 or x > 7):
+                print("Vui long nhap so 2 - 7")
+                retry = True
+            else:
+                retry = False
+        except ValueError:
+            print("Vui long nhap so")
+            retry = True
+    return x
+
+
+
 import math
 class dien_tich():
     s = 0
